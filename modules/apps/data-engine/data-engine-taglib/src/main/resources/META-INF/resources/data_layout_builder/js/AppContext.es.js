c9770cd493e5ec1817990c679d914446da958bc7
Keven <keven.leone@liferay.com>
2020-11-04 10:20:46 -0300

    LPS-122823 Add defaultValue in availableLanguageIds context

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/AppContext.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/AppContext.es.js
@@ -58,7 +58,7 @@ const initialState = {
 		unimplementedProperties: [],
 	},
 	dataDefinition: {
-		availableLanguageIds: [],
+		availableLanguageIds: [themeDisplay.getLanguageId()],
 		dataDefinitionFields: [],
 		name: {},
 	},
