7878739c7c4cc3d3d3993fe7dbf5e6cbc58dded6
Keven Leone <keven.leone@liferay.com>
2023-08-24 16:44:14 -0300

    LPS-189386 Create Pages Folder and Update main.jsx

--- workspaces/liferay-ticket-workspace/client-extensions/liferay-ticket-custom-element/src/main.jsx
+++ workspaces/liferay-ticket-workspace/client-extensions/liferay-ticket-custom-element/src/main.jsx
@@ -1,23 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 import React from 'react';
-import {createRoot} from 'react-dom/client';
-import TicketApp from './TicketApp.jsx';
-import {QueryClient, QueryClientProvider} from 'react-query';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import TicketApp from './pages/TicketApp';
 
 const queryClient = new QueryClient();
-class WebComponent extends HTMLElement {
-	connectedCallback() {
-		const root = createRoot(this);
-		root.render(
-			<React.StrictMode>
-				<QueryClientProvider client={queryClient}>
-					<TicketApp queryClient={queryClient} route={this.getAttribute('route') || '/'} />
-				</QueryClientProvider>
-			</React.StrictMode>
-		);
-	}
+class TicketWebComponent extends HTMLElement {
+  connectedCallback() {
+    const root = createRoot(this);
+
+    root.render(
+      <QueryClientProvider client={queryClient}>
+        <TicketApp queryClient={queryClient} />
+      </QueryClientProvider>
+    );
+  }
 }
+
 const ELEMENT_ID = 'liferay-ticket-custom-element';
 
 if (!customElements.get(ELEMENT_ID)) {
-	customElements.define(ELEMENT_ID, WebComponent);
+  customElements.define(ELEMENT_ID, TicketWebComponent);
 }
