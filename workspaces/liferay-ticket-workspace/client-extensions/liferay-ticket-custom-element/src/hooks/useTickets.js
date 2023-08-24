91d2850a3869414978df4a566f4997782d6ca10c
Keven Leone <keven.leone@liferay.com>
2023-08-24 16:56:52 -0300

    LPS-189386 Copyright on top

--- workspaces/liferay-ticket-workspace/client-extensions/liferay-ticket-custom-element/src/hooks/useTickets.js
+++ workspaces/liferay-ticket-workspace/client-extensions/liferay-ticket-custom-element/src/hooks/useTickets.js
@@ -1,12 +1,13 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 import {useMemo} from 'react';
 import {useQuery} from 'react-query';
 
 import {fetchTickets} from '../services/tickets';
 
-/**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
 const useTickets = (page, pageSize, filter, search) => {
 	const tickets = useQuery(
 		['tickets', {filter, page, pageSize, search}],
