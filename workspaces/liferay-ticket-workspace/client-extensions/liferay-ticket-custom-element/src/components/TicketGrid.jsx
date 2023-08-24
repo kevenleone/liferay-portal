7df078417a76942dbaf3d3ca28ff9a8e6997290e
Keven Leone <keven.leone@liferay.com>
2023-08-24 16:43:50 -0300

    LPS-189386 Create Components

---
+++ workspaces/liferay-ticket-workspace/client-extensions/liferay-ticket-custom-element/src/components/TicketGrid.jsx
@@ -0,0 +1,75 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayIcon from '@clayui/icon';
+import DataGrid from 'react-data-grid';
+
+import 'react-data-grid/lib/styles.css';
+
+const TicketGrid = ({ tickets }) => (
+  <DataGrid
+    columns={[
+      {
+        formatter: ({ row }) => (
+          <span>
+            {!!row.suggestions.length && (
+              <ClayIcon
+                className="mr-1"
+                spritemap={Liferay.Icons.spritemap}
+                symbol="link"
+              />
+            )}
+            {row.subject}
+          </span>
+        ),
+        key: 'subject',
+        name: 'Subject',
+        resizable: true,
+        width: '45%',
+      },
+      {
+        key: 'resolution',
+        name: 'Resolution',
+        resizable: true,
+        width: '15%',
+      },
+      {
+        formatter: ({ row }) => (
+          <span>
+            {row.ticketStatus === 'Queued' && (
+              <ClayIcon
+                className="mr-1"
+                spritemap={Liferay.Icons.spritemap}
+                symbol="bolt"
+              />
+            )}
+            {row.ticketStatus}
+          </span>
+        ),
+        key: 'ticketStatus',
+        name: 'Status',
+        resizable: true,
+        width: '15%',
+      },
+      {
+        key: 'priority',
+        name: 'Priority',
+        resizable: true,
+        width: '10%',
+      },
+      { key: 'type', name: 'Type', resizable: true, width: '10%' },
+      {
+        key: 'region',
+        name: 'Region',
+        resizable: true,
+        width: '10%',
+      },
+    ]}
+    rowKeyGetter={(row) => row.id}
+    rows={tickets.rows}
+  />
+);
+
+export { TicketGrid };
